7c37a1d9f2d0509b115c615f0ba14a0f473d6dd7
krishpranav <krisna.pranav@gmail.com>
2021-12-23 17:58:55 +0530

    static html

--- static_html/src/main.rs
+++ static_html/src/main.rs
@@ -1,10 +1,11 @@
 use kingkong::prelude::*;
 
 routes! {
-    GET "/index" => |_| Response::from_asset("index.html");
-    GET "/" => |_| "Hello World";
+    GET "/" => |_| Response::from_asset("index.html");
+    // GET "/" => |_| "Hello World";
 }
 
 fn main() {
+    asset_dir!("../assets");
     run!().unwrap();
 }
